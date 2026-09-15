ad29424c55103e0bbca3e6a60cd1f4b0750ce7c0
Davies Wabuluka <dwabuluka@gmail.com>
2026-09-15 18:46:20 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
+import Gallery from "@/components/Gallery";
 import Seasons from "@/components/Seasons";
 import ServiceArea from "@/components/ServiceArea";
 import About from "@/components/About";
@@ -15,6 +16,7 @@ export default function Home() {
       <main className="flex-1">
         <Hero />
         <Services />
+        <Gallery />
         <Seasons />
         <ServiceArea />
         <About />
